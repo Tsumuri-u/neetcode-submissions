9e2d4371424a066af3f8239e52a8e76faad835d6
Tsumuri <68445671+Tsumuri-u@users.noreply.github.com>
2026-05-22 02:45:19 -0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,36 @@
+#include <string>
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string output;
+        for (string str : strs) {
+
+            for (char c : str) {
+                output += to_string(((int)c));
+                output += ',';
+            }
+            output += ';';
+        }
+        cout << output << "\n";
+        return output;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> output;
+        string word = "";
+        string letter = "";
+        for (char c : s) {
+            if (c == ';') {
+                output.push_back(word);
+                word = "";
+            } else if (c == ',') {
+                word += (char)std::stoi(letter);
+                letter = "";
+            } else {
+                letter += c;
+            }
+        }
+        return output;
+    }
+};
