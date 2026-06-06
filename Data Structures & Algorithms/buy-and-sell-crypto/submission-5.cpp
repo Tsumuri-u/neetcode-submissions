6a9d05a8d2b5650efb9f76c1f6790d05e1235af4
Tsumuri <68445671+Tsumuri-u@users.noreply.github.com>
2026-06-05 19:37:47 -0700

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int left = 0;
+        int right = 1;
+        while (right < prices.size()) {
+            profit = max(profit, prices[right] - prices[left]);
+            if (prices[right] < prices[left]) {
+                left = right;
+                right = left + 1;
+            } else {
+                right++;
+            }  
+        }
+        return profit;
+    }
+};
