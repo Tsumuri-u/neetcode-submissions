cd8f732ee9f45399dc24d3399fcfb4eed57583b0
Tsumuri <68445671+Tsumuri-u@users.noreply.github.com>
2026-06-07 20:03:37 -0700

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> seen;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                seen.push(c);
+            }
+            if (c == ')' || c == '}' || c == ']') {
+                if (!seen.empty() && (seen.top() == c-1 || seen.top() == c-2)) {
+                    seen.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return seen.empty() ? true : false;
+
+    }
+};
+
